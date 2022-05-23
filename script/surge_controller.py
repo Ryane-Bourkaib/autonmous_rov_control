bc7b0d51637424aefb451f503aa1d8ff63997f0f
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2022-05-23 21:11:30 +0200

    surge control pid without velocity

--- script/surge_controller.py
+++ script/surge_controller.py
@@ -4,24 +4,25 @@ from script.pid import PID
 from std_msgs.msg import Int16
 from std_msgs.msg import Float64
 from std_msgs.msg import Empty
+from std_msgs.msg import Float64MultiArray
 from geometry_msgs.msg import Twist
 import tf
 import math
 
 from PI_Controller import*
 
-class YawController:
+class SurgeController:
 
     def __init__(self):
 
-        self.sensor_sub = rospy.Subscriber("mavros/imu/data",Int16,self.sensor_callback)
+        self.sensor_sub = rospy.Subscriber("distance_sonar", Float64MultiArray, self.sensor_callback)
         self.reset_sub = rospy.Subscriber("controllers/reset", Empty, self.reset_callback)
-        self.desired_val_sub = rospy.Subscriber("controllers/yaw/desired", Float64, self.desired_val_callback)
-        self.pub = rospy.Publisher('controller/yaw/effort', Float64, queue_size=10)
+        self.desired_val_sub = rospy.Subscriber("controller/surge/desired", Float64, self.desired_val_callback)
+        self.pub = rospy.Publisher('controller/surge/effort', Float64, queue_size=10)
         
         self.init = False
 
-        self.startup_yaw = 0.0
+        self.startup_surge = 0.0
 
         self.step = 0.02
 
@@ -38,36 +39,29 @@ class YawController:
         self.desired_val = msg.data
 
     def get_params(self):
-        self.kp = rospy.get_param('kp', 0.0)
-        self.ki = rospy.get_param('ki', 0.0)
-        self.kd = rospy.get_param('kd', 0.0)
+        self.kp = rospy.get_param('controller/surge/kp', 0.0)
+        self.ki = rospy.get_param('controller/surge/ki', 0.0)
+        self.kd = rospy.get_param('controller/surge/kd', 0.0)
 
     def reset_callback(self, data):
         self.init = True
 
     def sensor_callback(self,data):
-        orientation = data.orientation
-        angular_velocity = data.angular_velocity
+        global pinger_confidence
+        global pinger_distance
+        global pinger_0
 
-        # extraction of yaw angle
-        q = [orientation.x, orientation.y, orientation.z, orientation.w]
-        euler = tf.transformations.euler_from_quaternion(q)
-        angle_yaw = euler[2]
+        pinger_distance = data.data[0]
+        pinger_confidence = data.data[1]
 
-        if (self.init):
-            # at 1st execution, init
-            self.startup_yaw = angle_yaw
-            self.init = False
+        surge = pinger_distance
 
-        yaw = (angle_yaw - self.startup_yaw) * 180/math.pi
-        r = angular_velocity.z * 180/math.pi
-
-        control_effort = self.controller.control(self.desired_val, yaw, r)
+        control_effort = self.controller.control(self.desired_val, surge)  #removed r
         self.pub.publish(Float64(control_effort))
 
 def main(args):
-  rospy.init_node('yaw_controller_node')
-  controller = YawController()
+  rospy.init_node('surge_controller_node')
+  controller = SurgeController()
   try:
     rospy.spin()
   except KeyboardInterrupt:
