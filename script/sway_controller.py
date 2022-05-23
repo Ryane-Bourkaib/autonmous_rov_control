1cb51b54977471ee063bfc5fd30cffaad993e05a
luisanch <luissanchezmtn@gmail.com>
2022-05-23 20:22:36 +0200

    file created

---
+++ script/sway_controller.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import rospy
+from script.pid import PID
+from std_msgs.msg import Int16
+from std_msgs.msg import Float64
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+import tf
+import math
+
+from PI_Controller import*
+
+class YawController:
+
+    def __init__(self):
+
+        self.sensor_sub = rospy.Subscriber("mavros/imu/data",Int16,self.sensor_callback)
+        self.reset_sub = rospy.Subscriber("controllers/reset", Empty, self.reset_callback)
+        self.desired_val_sub = rospy.Subscriber("controllers/sway/desired", Float64, self.desired_val_callback)
+        self.pub = rospy.Publisher('controller/sway/effort', Float64, queue_size=10)
+        
+        self.init = False
+
+        self.startup_sway = 0.0
+
+        self.step = 0.02
+
+        self.controller = PID()
+
+        self.get_params()
+        self.set_controller()
+
+    def set_controller(self):
+        self.controller.set_gains(self.kp, self.ki, self.kd)
+        self.controller.set_step(self.step)
+        
+    def desired_val_callback(self, msg):
+        self.desired_val = msg.data
+
+    def get_params(self):
+        self.kp = rospy.get_param('controller/sway/kp', 0.0)
+        self.ki = rospy.get_param('controller/sway/ki', 0.0)
+        self.kd = rospy.get_param('controller/sway/kd', 0.0)
+
+    def reset_callback(self, data):
+        self.init = True
+
+    def sensor_callback(self,data):
+        orientation = data.orientation
+        angular_velocity = data.angular_velocity
+
+        # extraction of sway angle
+        q = [orientation.x, orientation.y, orientation.z, orientation.w]
+        euler = tf.transformations.euler_from_quaternion(q)
+        angle_sway = euler[2]
+
+        if (self.init):
+            # at 1st execution, init
+            self.startup_sway = angle_sway
+            self.init = False
+
+        sway = (angle_sway - self.startup_sway) * 180/math.pi
+        r = angular_velocity.z * 180/math.pi
+
+        control_effort = self.controller.control(self.desired_val, sway, r)
+        self.pub.publish(Float64(control_effort))
+
+def main(args):
+  rospy.init_node('sway_controller_node')
+  controller = YawController()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
