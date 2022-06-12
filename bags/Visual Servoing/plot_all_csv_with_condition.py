52461560edee389460a343b6b400e9382b448b82
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2022-06-13 00:23:09 +0200

    done

--- bags/Visual Servoing/plot_all_csv_with_condition.py
+++ bags/Visual Servoing/plot_all_csv_with_condition.py
@@ -24,43 +24,31 @@ import numpy as np
 #         file = f
 #         break
 
-file = 'bonus_dist.csv'
-PATH = ''
+#['surge_sway_servoing_error.csv', 'vx_vy_wz_servoing_error.csv']
+file = 'surge_sway_servoing_error.csv'
+PATH = './'
 
 ### Read .csv file and append to list
 df_d = pd.read_csv(PATH + file, index_col = None)
-df_d['%time'] = (df_d['%time'] - df_d['%time'].iloc[0]) / 1e6   #nano second to ms
+df_d['%time'] = (df_d['%time'] - df_d['%time'].iloc[0]) / 1e9   #nano second to s
 
-df_pwm = pd.read_csv(PATH + file.replace("_dist", "_thrusters"), index_col = None).drop(
-                                                    columns=['field.channels0','field.channels1'])
-df_pwm['%time'] = (df_pwm['%time'] - df_pwm['%time'].iloc[0]) / 1e6 #nano second to ms
-
-
-
-fig, ax1 = plt.subplots()
-
-color1 = 'black'
-ax1.set_xlabel('Time (milli second)')
-ax1.set_ylabel('Distance (mm)', color=color1)
-ax1.plot(df_d['%time'],df_d['field0'], color=color1, label='Obs_distance') #, label=file.replace("depth_control_12v_", "").replace(".csv", "").replace("_Depth", "").replace("_no_wall_2", "").replace("kp_p", "kp"))
-ax1.tick_params(axis='y', labelcolor=color1)
-
-ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-color = 'tab:blue'
-ax2.set_ylabel('PWM', color=color)  # we already handled the x-label with ax1
-# Heave , Yaw ,Surge, Sway
-ax2.plot(df_pwm['%time'],df_pwm['field.channels2'], color='crimson', label='Heave',linewidth=0.5)
-ax2.plot(df_pwm['%time'],df_pwm['field.channels3'], color='blue', label='Yaw', linewidth=0.5)
-ax2.plot(df_pwm['%time'],df_pwm['field.channels4'], color='green', label='Surge')
-ax2.plot(df_pwm['%time'],df_pwm['field.channels5'], color='orange', label='Sway',linewidth=0.25)
-# ax2.tick_params(axis='y', labelcolor=color)
+df_error16 = pd.read_csv(PATH + file, index_col = None).drop( columns=['%time','field.layout.data_offset'])
+np_error16 = df_error16.to_numpy().reshape((1800, -1, 2))
+print(np_error16.shape)
+np_error16_n = np.linalg.norm(np_error16, axis=(1,2))
+print(np_error16_n)
 
 
-ax1.legend()
-ax2.legend()
+color = 'tab:red'
+plt.xlabel('Time in seconds')
+plt.ylabel('||Error||', color=color)
+plt.plot(df_d['%time']+25,np_error16_n, color=color) #17.2
+plt.tick_params(axis='y', labelcolor=color)
 
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+
+
+
+plt.tight_layout()  # otherwise the right y-label is slightly clipped
 
 ### Generate the plot
 # plt.ylabel("Depth(m)")
